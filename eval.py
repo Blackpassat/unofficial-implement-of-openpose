0b4074baea34f5f6144237dbe65f6b32ab66132a
Shiyu Wang <shiyuwang@Jennifers-MacBook-Pro-3.local>
2019-04-16 15:54:22 -0400

    add evaluation, current stable 4.16

--- eval.py
+++ eval.py
@@ -1,36 +1,33 @@
-import sys
-import os
-import time
-from collections import OrderedDict
-
-import numpy as np
-import logging
 import argparse
-import json, re
-from tqdm import tqdm
-
-from common import read_imgfile
-from estimator import TfPoseEstimator
-# from networks import model_wh, get_graph_path
+import tensorflow as tf
+import sys
+import time
+import logging
+import cv2
+import numpy as np
+from tensorflow.contrib import slim
+import vgg
+import mobilenet_v2
+import mobilenet
+from cpm import PafNet
+import common
+from tensblur.smoother import Smoother
+from estimator import PoseEstimator, TfPoseEstimator
+import pdb
+import glob
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
+from tqdm import tqdm
+import os
+import json
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
-
-logger = logging.getLogger('TfPoseEstimator-Video')
-logger.setLevel(logging.INFO)
+logger = logging.getLogger('run')
+logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
-
-eval_size = -1
-
-
-def round_int(val):
-    return int(round(val))
-
 
 def write_coco_json(human, image_w, image_h):
     keypoints = []
@@ -43,124 +40,136 @@ def write_coco_json(human, image_w, image_h):
         keypoints.extend([round_int(body_part.x * image_w), round_int(body_part.y * image_h), 2])
     return keypoints
 
+def round_int(val):
+    return int(round(val))
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Tensorflow Openpose Inference')
-    parser.add_argument('--resize', type=str, default='0x0', help='if provided, resize images before they are processed. default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
-    parser.add_argument('--resize-out-ratio', type=float, default=8.0, help='if provided, resize heatmaps before they are post-processed. default=8.0')
-    # TODO
-    parser.add_argument('--modelname', type=str, default='model-6000.pb')
-    parser.add_argument('--cocoyear', type=str, default='2017')
-    parser.add_argument('--coco-dir', type=str, default='./COCO/')
-    parser.add_argument('--data-idx', type=int, default=-1)
-    parser.add_argument('--multi-scale', type=bool, default=False)
+    parser = argparse.ArgumentParser(description='Testing code for Openpose using Tensorflow')
+    parser.add_argument('--checkpoint_path', type=str, default='checkpoints/train/2019-3-23-19-39-4/')
+    parser.add_argument('--backbone_net_ckpt_path', type=str, default='checkpoints/mobilenet/mobilenet_v2_1.0_96.ckpt')
+    parser.add_argument('--train_mobilenet', type=bool, default=True)
+    parser.add_argument('--use_bn', type=bool, default=False)
+    parser.add_argument('--image_path', type=str, default='./COCO/images/val2017/')
+
     args = parser.parse_args()
 
-    cocoyear_list = ['2014', '2017']
-    if args.cocoyear not in cocoyear_list:
-        logger.error('cocoyear should be one of %s' % str(cocoyear_list))
-        sys.exit(-1)
+    checkpoint_path = args.checkpoint_path
+    logger.info('checkpoint_path: ' + checkpoint_path)
 
-    # TODO : Scales
+    with tf.name_scope('inputs'):
+        raw_img = tf.placeholder(tf.float32, shape=[None, 619, 654, 3])
+        img_size = tf.placeholder(dtype=tf.int32, shape=(2,), name='original_image_size')
 
-    image_dir = args.coco_dir + 'val%s/' % args.cocoyear
-    coco_json_file = args.coco_dir + 'annotations/person_keypoints_val%s.json' % args.cocoyear
-    cocoGt = COCO(coco_json_file)
-    catIds = cocoGt.getCatIds(catNms=['person'])
-    keys = cocoGt.getImgIds(catIds=catIds)
-    if args.data_idx < 0:
-        if eval_size > 0:
-            keys = keys[:eval_size]  # only use the first #eval_size elements.
-        pass
-    else:
-        keys = [keys[args.data_idx]]
-    logger.info('validation %s set size=%d' % (coco_json_file, len(keys)))
-    write_json = '../etcs/%s_%s_%0.1f.json' % (args.model, args.resize, args.resize_out_ratio)
+    img_normalized = raw_img / 255 - 0.5
 
-    logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
-    w, h = model_wh(args.resize)
-    if w == 0 or h == 0:
-        e = TfPoseEstimator(args.modelname, target_size=(432, 368))
-    else:
-        e = TfPoseEstimator(args.modelname, target_size=(w, h))
+    layers = {}
+    name = ""
+    with tf.contrib.slim.arg_scope(mobilenet_v2.training_scope()):
+        logits, endpoints = mobilenet_v2.mobilenet(img_normalized)
+        for k, tensor in sorted(list(endpoints.items()), key=lambda x: x[0]):
+            layers['%s%s' % (name, k)] = tensor
+            print(k, tensor.shape)
+    def upsample(input, target):
+        return tf.image.resize_bilinear(input, tf.constant([target.shape[1].value, target.shape[2].value]), align_corners=False)
+    
+    mobilenet_feature = tf.concat([layers['layer_7/output'], upsample(layers['layer_14/output'], layers['layer_7/output'])], 3)
+    
+    # get net graph
+    logger.info('initializing model...')
+    # net = PafNet(inputs_x=vgg_outputs, use_bn=args.use_bn)
+    # hm_pre, cpm_pre, added_layers_out = net.gen_net()
+    net = PafNet(inputs_x=mobilenet_feature, stage_num=6, hm_channel_num=19, use_bn=args.use_bn)
+    hm_pre, paf_pre, added_layers_out = net.gen_net()
 
-    print('FLOPs: ', e.get_flops())
+    hm_up = tf.image.resize_area(hm_pre[5], img_size)
+    cpm_up = tf.image.resize_area(paf_pre[5], img_size)
+    # hm_up = hm_pre[5]
+    # cpm_up = cpm_pre[5]
+    smoother = Smoother({'data': hm_up}, 25, 3.0)
+    gaussian_heatMat = smoother.get_output()
 
-    result = []
-    tqdm_keys = tqdm(keys)
-    for i, k in enumerate(tqdm_keys):
-        img_meta = cocoGt.loadImgs(k)[0]
-        img_idx = img_meta['id']
+    max_pooled_in_tensor = tf.nn.pool(gaussian_heatMat, window_shape=(3, 3), pooling_type='MAX', padding='SAME')
+    tensor_peaks = tf.where(tf.equal(gaussian_heatMat, max_pooled_in_tensor), gaussian_heatMat,
+                                 tf.zeros_like(gaussian_heatMat))
 
-        img_name = os.path.join(image_dir, img_meta['file_name'])
-        image = read_imgfile(img_name, None, None)
-        if image is None:
-            logger.error('image not found, path=%s' % img_name)
-            sys.exit(-1)
+    logger.info('initialize saver...')
+    # trainable_var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='openpose_layers')
+    # trainable_var_list = []
+    trainable_var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='openpose_layers')
+    if args.train_mobilenet:
+        trainable_var_list = trainable_var_list + tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='MobilenetV2')
 
-        # inference the image with the specified network
-        t = time.time()
-        humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
-        elapsed = time.time() - t
+    restorer = tf.train.Saver(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='MobilenetV2'), name='mobilenet_restorer')
+    saver = tf.train.Saver(trainable_var_list)
+    logger.info('initialize session...')
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
 
-        scores = 0
-        ann_idx = cocoGt.getAnnIds(imgIds=[img_idx], catIds=[1])
-        anns = cocoGt.loadAnns(ann_idx)
-        for human in humans:
-            item = {
-                'image_id': img_idx,
-                'category_id': 1,
-                'keypoints': write_coco_json(human, img_meta['width'], img_meta['height']),
-                'score': human.score
-            }
-            result.append(item)
-            scores += item['score']
+    with tf.Session(config=config) as sess:
+        sess.run(tf.group(tf.global_variables_initializer()))
+        logger.info('restoring mobilenet weights...')
+        restorer.restore(sess, args.backbone_net_ckpt_path)
+        logger.info('restoring from checkpoint...')
+        saver.restore(sess, "./checkpoints/train/2019-3-23-19-39-4/model-12000")
+        #saver.restore(sess, args.checkpoint_path + "model-8.ckpt")
+        logger.info('initialization done')
 
-        avg_score = scores / len(humans) if len(humans) > 0 else 0
-        tqdm_keys.set_postfix(OrderedDict({'inference time': elapsed, 'score': avg_score}))
-        if args.data_idx >= 0:
-            logger.info('score:', k, len(humans), len(anns), avg_score)
+        ## variables for file namess etc
+        result = []
+        write_json = "result.json"
 
-            import matplotlib.pyplot as plt
-            fig = plt.figure()
-            a = fig.add_subplot(2, 3, 1)
-            plt.imshow(e.draw_humans(image, humans, True))
+        image_dir = args.image_path
+        coco_json_file = './COCO/annotations/person_keypoints_val2017.json'
+        cocoGt = COCO(coco_json_file)
+        catIds = cocoGt.getCatIds(catNms=['person'])
+        keys = cocoGt.getImgIds(catIds=catIds)
+        tqdm_keys = tqdm(keys)
+        for i, k in enumerate(tqdm_keys):
+            img_meta = cocoGt.loadImgs(k)[0]    
+            img_idx = img_meta['id']
 
-            a = fig.add_subplot(2, 3, 2)
-            # plt.imshow(cv2.resize(image, (e.heatMat.shape[1], e.heatMat.shape[0])), alpha=0.5)
-            tmp = np.amax(e.heatMat[:, :, :-1], axis=2)
-            plt.imshow(tmp, cmap=plt.cm.gray, alpha=0.5)
-            plt.colorbar()
+            img_name = os.path.join(image_dir, img_meta['file_name'])
+            input_image = common.read_imgfile(img_name, None, None)
 
-            tmp2 = e.pafMat.transpose((2, 0, 1))
-            tmp2_odd = np.amax(np.absolute(tmp2[::2, :, :]), axis=0)
-            tmp2_even = np.amax(np.absolute(tmp2[1::2, :, :]), axis=0)
+            size = [input_image.shape[0], input_image.shape[1]]
+            if input_image is None:
+                logger.error('Image can not be read, path=%s' % input_image)
+                sys.exit(-1)        
+            # h = int(654 * (size[0] / size[1]))
+            img = np.array(cv2.resize(input_image, (654, 619)))
+            # cv2.imshow('ini', img)
+            img = img[np.newaxis, :]
+            peaks, heatmap, vectormap = sess.run([tensor_peaks, hm_up, cpm_up], feed_dict={raw_img: img, img_size: size})
+            # cv2.imshow('in', vectormap[0, :, :, 0])
+            humans = PoseEstimator.estimate_paf(peaks[0], heatmap[0], vectormap[0])
 
-            a = fig.add_subplot(2, 3, 4)
-            a.set_title('Vectormap-x')
-            # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
-            plt.imshow(tmp2_odd, cmap=plt.cm.gray, alpha=0.5)
-            plt.colorbar()
+            for human in humans:
+                item = {
+                    'image_id': img_idx,
+                    'category_id': 1,
+                    'keypoints': write_coco_json(human, img_meta['width'], img_meta['height']),
+                    'score': human.score
+                }
+                result.append(item)
 
-            a = fig.add_subplot(2, 3, 5)
-            a.set_title('Vectormap-y')
-            # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
-            plt.imshow(tmp2_even, cmap=plt.cm.gray, alpha=0.5)
-            plt.colorbar()
+        fp = open(write_json, 'w')
+        json.dump(result, fp)
+        fp.close()
 
-            plt.show()
+        # image = TfPoseEstimator.draw_humans(image, bodys, imgcopy=False)
+        # # cv2.imshow(' ', image)
+        # cv2.imwrite("result.png", image)
+        # cv2.waitKey(0)
+        
+        ### call COCO apis for json file evaluation
+        cocoDt = cocoGt.loadRes(write_json)
+        cocoEval = COCOeval(cocoGt, cocoDt, 'keypoints')
+        cocoEval.params.imgIds = keys
+        cocoEval.evaluate()
+        cocoEval.accumulate()
+        cocoEval.summarize()
 
-    fp = open(write_json, 'w')
-    json.dump(result, fp)
-    fp.close()
+        print(''.join(["%11.4f |" % x for x in cocoEval.stats]))
 
-    cocoDt = cocoGt.loadRes(write_json)
-    cocoEval = COCOeval(cocoGt, cocoDt, 'keypoints')
-    cocoEval.params.imgIds = keys
-    cocoEval.evaluate()
-    cocoEval.accumulate()
-    cocoEval.summarize()
-
-    print(''.join(["%11.4f |" % x for x in cocoEval.stats]))
-
-    pred = json.load(open(write_json, 'r'))
+        pred = json.load(open(write_json, 'r'))
+        pdb.set_trace()
